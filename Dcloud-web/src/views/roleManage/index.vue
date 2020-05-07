3794f76f21c4549b28697d63a846ca3cccbe4f82
sylviareset <1052251409@qq.com>
2020-05-07 22:14:00 +0800

    角色管理
    角色管理

--- Dcloud-web/src/views/roleManage/index.vue
+++ Dcloud-web/src/views/roleManage/index.vue
@@ -14,8 +14,8 @@
         </el-form-item>
         <el-form-item label="状态：" style="float:right">
           <el-select v-model="formInline.state" placeholder="请选择状态" size="small">
-            <el-option label="已禁用" value="1"></el-option>
-            <el-option label="已启用" value="2"></el-option>
+            <el-option label="禁用" value="1"></el-option>
+            <el-option label="启用" value="2"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="角色名称：" style="float:right">
@@ -37,22 +37,22 @@
         <el-table-column label="序号" type="index" width="50" align="center"></el-table-column>
         <el-table-column label="角色名称" min-width="110" align="center">
           <template slot-scope="scope">
-            <span>{{scope.row.username}}</span>
+            <span>{{scope.row.name}}</span>
           </template>
         </el-table-column>
         <el-table-column label="备注" min-width="110" align="center">
           <template slot-scope="scope">
-            <span>{{scope.row.note}}</span>
+            <span>{{scope.row.description}}</span>
           </template>
         </el-table-column>
         <el-table-column label="角色状态" min-width="50" align="center">
           <template slot-scope="scope">
             <el-button
-              :type="scope.row.state"
+              :type="btnState(scope.row.state)"
               plain
               @click="changeState(scope.row)"
               size="small"
-            >{{scope.row.stateName}}</el-button>
+            >{{filterState(scope.row.state)}}</el-button>
           </template>
         </el-table-column>
         <el-table-column align="center" label="操作" min-width="100">
@@ -79,29 +79,34 @@
     </div>
     <el-dialog :title="title" :visible.sync="dialogFormVisible">
       <el-form
-        :model="ruleForm"
+        :model="roleForm"
         :rules="rules"
-        ref="ruleForm"
+        ref="roleForm"
         label-width="100px"
-        class="demo-ruleForm"
+        class="demo-roleForm"
       >
-        <el-form-item label="角色名称" prop="username">
-          <el-input v-model="ruleForm.username"></el-input>
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="roleForm.name"></el-input>
         </el-form-item>
 
         <el-form-item label="角色状态" v-if="title=='编辑角色'">
-          <el-radio v-model="ruleForm.type" label="0">禁用</el-radio>
-          <el-radio v-model="ruleForm.type" label="1">启用</el-radio>
+          <el-radio v-model="roleForm.state" label="0">禁用</el-radio>
+          <el-radio v-model="roleForm.state" label="1">启用</el-radio>
         </el-form-item>
         <el-form-item label="备注">
-          <el-input v-model="ruleForm.note" type="textarea" :rows="4"></el-input>
+          <el-input v-model="roleForm.description" type="textarea" :rows="4"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer" style="text-align: center;margin-bottom:10px">
-        <el-button type="primary" @click="submitForm('ruleForm')" style="width:180px">确定</el-button>
-        <el-button @click="resetForm('ruleForm')" style="width:180px">取消</el-button>
+        <el-button type="primary" @click="submitForm('roleForm')" style="width:180px">确定</el-button>
+        <el-button @click="resetForm('roleForm')" style="width:180px">取消</el-button>
       </div>
-      <div slot="footer" class="dialog-footer" style="text-align: center;color: #5d5b5b;font-size:14px" v-if="title=='新增角色'">
+      <div
+        slot="footer"
+        class="dialog-footer"
+        style="text-align: center;color: #5d5b5b;font-size:14px"
+        v-if="title=='新增角色'"
+      >
         <span>新增角色默认为已启用</span>
       </div>
     </el-dialog>
@@ -112,39 +117,30 @@
 export default {
   data() {
     return {
-      list: [
-        {
-          username: "超管",
-          note: "超级管理员",
-          type: "1",
-          state: "primary",
-          stateName: "已启用"
-        }
-      ],
+      list: [],
       AllData: [],
       listLoading: false,
       dialogFormVisible: false,
-      ruleForm: {
-        username: "",
-        type: "0",
-        note: ""
+      roleForm: {
+        name: "",
+        state: "1",
+        description: ""
       },
       rules: {
-        username: [
-          { required: true, message: "请输入角色名称", trigger: "blur" }
-        ]
+        name: [{ required: true, message: "请输入角色名称", trigger: "blur" }]
       },
-      totalNum: 1,
+      totalNum: 1, //0
       title: "新增角色",
       pageSize: 10,
       formInline: {
         role: "",
         state: ""
-      }
+      },
+      page: 1
     };
   },
   created() {
-    // this.showUserInfo();
+    this.showRoleInfo();
   },
   methods: {
     showData() {
@@ -163,27 +159,69 @@ export default {
       //搜索
       this.list = [];
       this.listLoading = true;
+      this.page = 1;
+      var data = {
+        page: this.page,
+        name: this.formInline.role,
+        state: this.formInline.state
+      };
+      this.$http
+        .get(
+          "/api/role?page=" +
+            data.page +
+            "&name=" +
+            data.name +
+            "&state=" +
+            data.state
+        )
+        .then(res => {
+          this.listLoading = false;
+          this.totalNum = res.data.total;
+          if (this.totalNum != 0) {
+            delete res.data[0];
+            this.list = res.data.records;
+          }
+        });
     },
-    resetData() {},
+    resetData() {
+      this.formInline.role="";
+      this.formInline.state="";
+      this.showRoleInfo(this.page)
+    },
     reset() {
-      this.ruleForm.username = "";
-      this.ruleForm.note = "";
+      this.roleForm.username = "";
+      this.roleForm.note = "";
     },
-    changeState(row) {
-      if (row.type == "1") {
-        row.state = "danger";
-        row.stateName = "已禁用";
-        row.type = "0";
+    btnState(state) {
+      if (state == "1") {
+        return "primary";
       } else {
-        row.state = "primary";
-        row.stateName = "已启用";
-        row.type = "1";
+        return "danger";
       }
     },
+    filterState(state) {
+      if (state == "1") {
+        return "启用";
+      } else {
+        return "禁用";
+      }
+    },
+    changeState(row) {
+      this.$http.patch("/api/role?id=" + row.id+"&state="+row.state).then(res => {
+      if (row.type == "1") {
+        row.state = "0";
+        row.stateName = "禁用";
+        row.type = "0";
+      } else {
+        row.state = "1";
+        row.stateName = "启用";
+        row.type = "1";
+      }
+      });
+    },
     editData(row) {
-      this.ruleForm.username = "";
-      this.ruleForm.note = "";
-      this.ruleForm.type = row.type;
+      this.roleForm = row;
+      this.roleForm.state = this.roleForm.state.toString();
       this.dialogFormVisible = true;
       this.title = "编辑角色";
     },
@@ -194,14 +232,64 @@ export default {
         cancelButtonText: "取消",
         type: "info"
       })
-        .then(() => {})
+        .then(() => {
+          this.$http.delete("/api/role?id=" + row.id).then(res => {
+            if (res.data.respCode == "1") {
+              this.$alert("删除成功", "成功", {
+                confirmButtonText: "确定"
+              });
+            }
+            this.showRoleInfo(this.page);
+          });
+        })
         .catch(() => {});
     },
 
     submitForm(formName) {
+      var data={
+        id:this.roleForm.id,
+        description: this.roleForm.description,
+        name: this.roleForm.name,
+        state:this.roleForm.state
+      }
+      var addData = {
+        description: this.roleForm.description,
+        name: this.roleForm.name
+      };
       this.$refs[formName].validate(valid => {
         if (valid) {
           this.dialogFormVisible = false;
+          if (this.title == "新增角色") {
+            this.$http.post("/api/role", addData).then(res => {
+              if (res.data.respCode == "1") {
+                this.$alert("角色新增成功", "成功", {
+                  confirmButtonText: "确定"
+                });
+                this.showRoleInfo(this.page);
+              } else {
+                this.$alert("角色新增失败", "失败", {
+                  confirmButtonText: "确定"
+                });
+                this.showRoleInfo(this.page);
+              }
+              this.$refs[formName].resetFields();
+            });
+          } else {
+             this.$http.put("/api/role", data).then(res => {
+              if (res.data.respCode == "1") {
+                this.$alert("角色修改成功", "成功", {
+                  confirmButtonText: "确定"
+                });
+                this.showRoleInfo(this.page);
+              } else {
+                this.$alert("角色修改失败", "失败", {
+                  confirmButtonText: "确定"
+                });
+                this.showRoleInfo(this.page);
+              }
+              this.$refs[formName].resetFields();
+            });
+          }
         }
       });
     },
@@ -209,6 +297,36 @@ export default {
       this.dialogFormVisible = false;
       this.$refs[formName].resetFields();
       this.reset();
+    },
+    showRoleInfo(page) {
+      this.list = [];
+      this.listLoading = true;
+      this.page = 1;
+      var name = "";
+      var state = "";
+      //获取角色信息
+      var data = {
+        page: this.page,
+        name: name,
+        state: state
+      };
+      this.$http
+        .get(
+          "/api/role?page=" +
+            data.page +
+            "&name=" +
+            data.name +
+            "&state=" +
+            data.state
+        )
+        .then(res => {
+          this.listLoading = false;
+          this.totalNum = res.data.total;
+          if (this.totalNum != 0) {
+            delete res.data[0];
+            this.list = res.data.records;
+          }
+        });
     },
     handleCurrentChange(val) {
       // console.log(val);
@@ -230,8 +348,21 @@ export default {
         }
       }
     },
-    permissionAssign(row){
-     this.$router.push({path:"/roleManage/permissionAssignment",query:{username:row.username}});
+    permissionAssign(row) {
+      var description=[];
+      this.$http
+        .get(
+          "/api/rolePower?role_id=" +row.id)
+        .then(res => {
+          description=res.data;
+          this.$router.push({
+        path: "/roleManage/permissionAssignment",
+        query: { username: row.name,
+        id:row.id,
+        description
+        }
+      });
+        });
     }
   }
 };
@@ -241,7 +372,6 @@ export default {
   text-align: center !important;
   margin: 20px 0 !important;
 }
-
 </style>
 <style>
 .el-table__header {
--- Dcloud-web/src/views/roleManage/permissionAssignment.vue
+++ Dcloud-web/src/views/roleManage/permissionAssignment.vue
@@ -33,7 +33,9 @@
         <el-checkbox label="3">删除菜单</el-checkbox>
         <el-checkbox label="4">查询菜单</el-checkbox>
       </el-checkbox-group>
+
       <el-divider></el-divider>
+      <!--
       <h5>班课管理</h5>
       <el-checkbox-group v-model="classList">
         <el-checkbox label="1">发起签到</el-checkbox>
@@ -41,9 +43,10 @@
         <el-checkbox label="3">创建活动</el-checkbox>
         <el-checkbox label="4">分配小组</el-checkbox>
       </el-checkbox-group>
+      -->
       <div class="foot">
-        <el-button type="primary" class="but">保存</el-button>
-        <el-button type="info" plain  class="but" @click="reset()">重置</el-button>
+        <el-button type="primary" class="but" @click="saveRolePer()">保存</el-button>
+        <el-button type="info" plain class="but" @click="reset()">重置</el-button>
       </div>
     </div>
   </div>
@@ -54,14 +57,77 @@ export default {
     return {
       checkList: [],
       menuList: [],
-      classList: []
+      classList: [],
+      perList: [],
+      saveList: ""
     };
   },
-  methods:{
-    reset(){
-      this.checkList=[];
-      this.menuList=[];
-      this.classList=[];
+  created() {
+    this.showRolePer();
+  },
+  methods: {
+    reset() {
+      this.checkList = [];
+      this.menuList = [];
+      this.classList = [];
+    },
+    showRolePer() {
+      this.perList = this.$route.query.description;
+      if (this.perList == 0) {
+        this.checkList = [];
+        this.menuList = [];
+      } else {
+        for (var i = 0; i < 12; i++) {
+          if (this.perList[i] != 0 && this.perList[i] <= 8) {
+            this.checkList.push(this.perList[i]);
+          }
+          if (this.perList[i] > 8) {
+            var menuNum = (this.perList[i] - 8).toString();
+            this.menuList.push(menuNum);
+          }
+        }
+      }
+    },
+    saveRolePer() {
+      if (this.checkList.length == 0 && this.menuList.length == 0) {
+        this.saveList = 0;
+      } else {
+        if (this.menuList.length != 0) {
+          for (var i = 0; i < this.menuList.length; i++) {
+            this.menuList[i] = (parseInt(this.menuList[i]) + 8).toString();
+          }
+        }
+        if (this.checkList.length != 0) {
+          this.saveList = this.checkList[0];
+          for (var i = 1; i < this.checkList.length; i++) {
+            this.saveList += ",";
+            this.saveList += this.checkList[i];
+          }
+        }
+        if (this.menuList.length != 0) {
+           this.saveList += ",";
+          this.saveList += this.menuList[0];
+          for (var i = 1; i < this.menuList.length; i++) {
+            this.saveList += ",";
+            this.saveList += this.menuList[i];
+          }
+        }
+      }
+      var power_id = this.saveList;
+      this.$http
+        .put("/api/rolePower?role_id=" + this.$route.query.id+"&power_id="+power_id)
+        .then(res => {
+          if (res.data.respCode == "1") {
+            this.$alert("保存成功", "成功", {
+              confirmButtonText: "确定"
+            });
+            this.$router.push("/roleManage");
+          } else {
+            this.$alert("保存失败", "失败", {
+              confirmButtonText: "确定"
+            });
+          }
+        });
     }
   }
 };
@@ -88,7 +154,7 @@ export default {
   text-align: center;
   margin-top: 30px;
 }
-.but{
+.but {
   width: 180px;
 }
 </style>
