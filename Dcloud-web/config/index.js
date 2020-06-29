f83c923090a7c652715f96c5eda6bb4bdf56e4b2
laixiaolian <laixiaolian@users.noreply.github.com>
2020-06-29 13:01:43 +0800

    发布环境转生产环境

--- Dcloud-web/config/index.js
+++ Dcloud-web/config/index.js
@@ -23,8 +23,8 @@ module.exports = {
         },
 
         // Various Dev Server settings
-        // host: 'localhost', // can be overwritten by process.env.HOST
-        host: '0.0.0.0',
+        host: 'localhost', // can be overwritten by process.env.HOST
+        // host: '0.0.0.0',
         port: 8081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: true,
         errorOverlay: true,
@@ -74,8 +74,8 @@ module.exports = {
          * then assetsPublicPath should be set to "/bar/".
          * In most cases please use '/' !!!
          */
-        // assetsPublicPath: '/admin/', // If you are deployed on the root path, please use '/'
-        assetsPublicPath: './',
+        assetsPublicPath: '/admin/', // If you are deployed on the root path, please use '/'
+        // assetsPublicPath: './',
 
         /**
          * Source Maps
